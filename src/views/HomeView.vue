d32afbf904b6ebbcce88b6763c658339ada529a9
Victor P <mfreke.victor@gmail.com>
2023-09-19 11:50:42 +0100

    Initial commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -169,16 +169,16 @@ input:focus {
 
 * {
   font-family: 'Inter', sans-serif;
-  font-weight: 500;
+  font-weight: 500!important;
 }
 
 .register {
-  font-weight: 600;
+  font-weight: 600!important;
   font-size: 32px;
 }
 
 .swap {
-  font-weight: 300;
+  font-weight: 300!important;
   margin-bottom: 20px;
 }
 
